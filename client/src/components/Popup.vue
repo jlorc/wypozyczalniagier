9b50359c7013d5ae2a397e78f414fc1082179a67
Jakub Lorc <lorc.jakub@gmail.com>
2019-11-07 00:12:55 +0100

    handle rent game form

--- client/src/components/Popup.vue
+++ client/src/components/Popup.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="popup" :class="{'active': show}">
     <div class="popup__blackout"></div>
-    <div class="popup__wrapper">
+    <div class="popup__wrapper" v-if="!showFormStep && !showThankYouStep">
       <div class="popup__content">
         <img :src="data.img_url" class="popup__game-image">
         <div class="popup__info">
@@ -24,9 +24,26 @@
       <p class="popup__game-description">{{ data.hardware_requirements }}</p>
       <span class="popup__close" @click="hidePopup"></span>
       <div class="popup__button-wrapper">
-        <span class="popup__button">Wypożycz</span>
+        <span class="popup__button" @click="showForm">Wypożycz</span>
         <p class="popup__game-description popup__game-description--button-info">Pozostałych sztuk: {{ data.quantity }}</p>
       </div>
+    </div>
+    <div class="popup__wrapper popup__wrapper--form" v-if="showFormStep && !showThankYouStep">
+      <h2 class="popup__form-title">Wypożycz grę</h2>
+      <p class="popup__form-description">Wypełnij poniższe pola, aby zarezerwować <span>{{ data.game_name }}</span> w naszym salonie.</p>
+      <div class="popup__form-content">
+        <input type="text" placeholder="Imię" v-model="name">
+        <input type="text" placeholder="Nazwisko" v-model="surname">
+        <input type="text" placeholder="Numer telefonu" v-model="number">
+      </div>
+      <span class="popup__error-label" v-if="error">Proszę uzupełnić wszystkie pola!</span>
+      <span class="popup__button popup__button--centered" @click="showThankYou">Wypożycz</span>
+      <span class="popup__close" @click="hidePopup"></span>
+    </div>
+    <div class="popup__wrapper popup__wrapper--thank-you" v-if="!showFormStep && showThankYouStep">
+      <h2 class="popup__form-title">Gratulacje!</h2>
+      <p class="popup__form-description">Udało Ci się zarezerwować grę, zapraszamy do salonu po jej odbiór!</p>
+      <span class="popup__close" @click="hidePopup"></span>
     </div>
   </div>
 </template>
@@ -38,6 +55,12 @@
       return {
         data: [],
         show: false,
+        showFormStep: false,
+				showThankYouStep: false,
+        name: null,
+        surname: null,
+        number: null,
+        error: false,
       };
     },
     mounted() {
@@ -52,8 +75,30 @@
         this.show = true;
         document.body.classList.add('no-scroll');
       },
+      showForm() {
+      	this.showFormStep = true;
+      },
+			showThankYou() {
+      	if (this.name && this.surname && this.number) {
+					this.showThankYouStep = true;
+					this.showFormStep = false;
+					this.error = false;
+					this.clearFormFields();
+        } else {
+      		this.error = true;
+        }
+      },
+      clearFormFields() {
+      	this.name = null;
+      	this.surname = null;
+      	this.number = null;
+      },
       hidePopup() {
       	this.show = false;
+      	setTimeout(() => {
+					this.showFormStep = false;
+					this.showThankYouStep = false;
+        }, 1000);
 				document.body.classList.remove('no-scroll');
       }
     }
@@ -79,6 +124,50 @@
       visibility: visible;
     }
 
+    &__error-label {
+      color: #fa2f44;
+      margin: 5px 0 35px;
+      font-size: 14px;
+      text-align: center;
+      width: 100%;
+    }
+
+    &__form {
+      &-title {
+        text-align: center;
+        width: 100%;
+        margin-bottom: 15px;
+      }
+
+      &-description {
+        text-align: center;
+      }
+
+      &-content {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        width: 100%;
+        padding: 20px 0 5px;
+
+        input {
+          width: 70%;
+          background: transparent;
+          border: none;
+          border-bottom: 1px solid black;
+          font-size: 18px;
+          padding-bottom: 5px;
+          color: black;
+          margin-bottom: 25px;
+
+          &:focus {
+            outline: none;
+          }
+        }
+      }
+    }
+
     &__button {
       display: block;
       color: white;
@@ -89,6 +178,10 @@
       font-size: 18px;
       cursor: pointer;
       transition: 0.3s ease all;
+
+      &--centered {
+        margin: auto;
+      }
 
       &-wrapper {
         display: flex;
@@ -224,6 +317,17 @@
       align-items: flex-start;
       flex-direction: column;
       position: relative;
+
+      &--form {
+        height: auto;
+        max-width: 500px;
+      }
+
+      &--thank-you {
+        height: auto;
+        max-width: 600px;
+        min-height: 140px;
+      }
     }
   }
 </style>
